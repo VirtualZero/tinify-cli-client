e5d054af71df1b9cc8a34aa09e711fa451b34fd3
Birtchum <birtchum@virtualzero.net>
2019-10-13 17:37:29 -0400

    Finished image scaling functionality

--- tiny-cli-client.py
+++ tiny-cli-client.py
@@ -12,7 +12,6 @@ valid_extensions = ['jpg', 'png', 'gif']
 
 def compress_image(image_path, output_path, new_name):
     image_name = image_path.strip('/').split('/')[-1]
-
     with Halo(
         text=f'Compressing {image_name} ...',
         spinner='dots',
@@ -25,18 +24,26 @@ def compress_image(image_path, output_path, new_name):
             if not new_name:
                 name = image_name.split('.')[0]
                 extension = image_name.split('.')[1]
-
                 path_regex = re.compile('^(.*/).*$')
-                save_to_path = os.path.join(
-                    path_regex.search(image_path).group(1),
-                    f'{name}-optimized.{extension}'
-                )
+                optimized_name = f'{name}-optimized.{extension}'
+
+                try:
+                    save_to_path = os.path.join(
+                        path_regex.search(image_path).group(1),
+                        optimized_name
+                    )
+
+                except AttributeError:
+                    save_to_path = optimized_name
                 
                 source.to_file(save_to_path)
 
             else:
                 path_regex = re.compile('^(.*/).*$')
-                save_to_path = f'{path_regex.search(image_path).group(1)}{new_name}'
+                save_to_path = os.path.join(
+                    path_regex.search(image_path).group(1),
+                    new_name
+                )
                 source.to_file(save_to_path)
 
         else:
@@ -110,13 +117,119 @@ def optimize_directory(directory, output_path):
 
     sys.exit(0)
 
+
+def verify_input(image, directory):
+    if not image and not directory:
+        print(
+            colored(
+                'You must specify either -i or -d option.',
+                'red'
+            )
+        )
+
+        sys.exit(1)
+
+
+def validate_output_path(output_path):
+    if not os.path.isdir(output_path):
+        print(
+            colored(
+                f'Output directory: {output_path} is not a directory.',
+                'red'
+            )
+        )
+
+        sys.exit(1)
+
+
+def validate_directory(directory):
+    if not os.path.isdir(directory):
+        print(
+            colored(
+                f'{directory} is not a directory',
+                'red'
+            )
+        )
+
+        sys.exit(1)
+
+
+def validate_image(image_path):
+    if not os.path.isfile(image_path):
+        print(
+            colored(
+                f'{image_path} is not a valid image path.',
+                'red'
+            )
+        )
+
+        sys.exit(1)
+
+    extension = image_path.split('.')[-1].lower()
+    image_name = image_path.strip('/').split('/')[-1]
+
+    if extension not in valid_extensions:
+        print(
+            colored(
+                f'{image_name} is not a valid image, must be .jpg, .png, or .gif.',
+                'red'
+            )
+        )
+
+        sys.exit(1)
+
+
+def validate_resize_options(args):
+    if not args.scale \
+        and not args.fit \
+        and not args.cover \
+        and not args.thumb:
+
+        print(
+            colored(
+                f'You must specify either -s, -f, -t, or -c option when resizing an image.',
+                'red'
+            )
+        )
+
+        sys.exit(1)
+
+
+def validate_height_and_width(args):
+    if args.scale:
+        if not args.height and not args.width:
+            print(
+                colored(
+                    f'You must specify either -H OR -w option when scaling an image.',
+                    'red'
+                )
+            )
+
+        if args.height and args.width:
+            print(
+                colored(
+                    f'You must specify ONLY -H OR -w option when scaling an image, not both.',
+                    'red'
+                )
+            )
+
+    elif args.fit or args.cover or args.thumb:
+        if not args.height or not args.width:
+            print(
+                colored(
+                    f'You must specify both -H AND -w options with this operation.',
+                    'red'
+                )
+            )
+
+
 def parse_args():
     parser = argparse.ArgumentParser(
         description="Manipulate images using the tinify API."
     )
 
     parser.add_argument(
-        '-c',
+        '-C',
         '--compress',
         help=f'Choose this option to compress images.',
         action='store_true'
@@ -126,14 +239,14 @@ def parse_args():
         '-i',
         '--image',
         help=f'Compresses and optimizes a single image. '\
-        f'Usage: python tiny.py -ci /path/to/image.png'
+        f'Usage: python tiny-cli-client.py -Ci /path/to/image.png'
     )
 
     parser.add_argument(
         '-d',
         '--directory',
         help=f'Compresses and optimizes all images within a directory. '\
-        f'Usage: python tiny.py -cd /path/to/directory'
+        f'Usage: python tiny-cli-client.py -Cd /path/to/directory'
     )
 
     parser.add_argument(
@@ -144,108 +257,196 @@ def parse_args():
         f'directory specified by the -d option. All optimized images are saved within. Default ' \
         f'action with the -i option is to append "-optimized" to the image name and save the optimized ' \
         f'image to the same directory as the source image. ' \
-        f'Usage: python tiny.py -ci /path/to/image.jpg -o /path/to/save/directory'
+        f'Usage: python tiny-cli-client.py -Ci /path/to/image.jpg -o /path/to/save/directory'
     )
 
     parser.add_argument(
         '-n',
         '--name',
         help=f'Rename the optimized image. DOES NOT WORK WITH -d OPTION. ' \
-        f'Usage: python tiny.py -ci /path/to/image.jpg -o /path/to/save/directory -n new-image-name.jpg'
+        f'Usage: python tiny-cli-client.py -Ci /path/to/image.jpg -o /path/to/save/directory -n new-image-name.jpg'
+    )
+
+    parser.add_argument(
+        '-R',
+        '--resize',
+        help=f'Choose this option to resize images. This option requires either -s, -f, -t, or -c options, ' \
+        f'as well as either the -H or -w options, or both. Refer to -s, -f, -t, and -c options for more on this.',
+        action='store_true'
+    )
+
+    parser.add_argument(
+        '-s',
+        '--scale',
+        help=f'Scale the optimized image(s) to a desired width or height while keeping aspect ratio. ' \
+        f'Requires either -w OR -H option, but not both. ' \
+        f'Usage: python tiny-cli-client.py -Rsi /path/to/image.jpg -w 125',
+        action='store_true'
+    )
+
+    parser.add_argument(
+        '-f',
+        '--fit',
+        help=f'Scale the optimized image(s) to a desired width or height while keeping aspect ratio. '
+        f'Requires either -w OR -H option, but not both. '
+        f'Usage: python tiny-cli-client.py -Rsi /path/to/image.jpg -w 125',
+        action='store_true'
+    )
+
+    parser.add_argument(
+        '-t',
+        '--thumb',
+        help=f'Scale the optimized image(s) to a desired width or height while keeping aspect ratio. '
+        f'Requires either -w OR -H option, but not both. '
+        f'Usage: python tiny-cli-client.py -Rsi /path/to/image.jpg -w 125',
+        action='store_true'
+    )
+
+    parser.add_argument(
+        '-c',
+        '--cover',
+        help=f'Scale the optimized image(s) to a desired width or height while keeping aspect ratio. '
+        f'Requires either -w OR -H option, but not both. '
+        f'Usage: python tiny-cli-client.py -Rsi /path/to/image.jpg -w 125',
+        action='store_true'
+    )
+
+    parser.add_argument(
+        '-H',
+        '--height',
+        help=f'The desired height in PIXELS of the image(s) when resizing.'
+    )
+
+    parser.add_argument(
+        '-w',
+        '--width',
+        help=f'The desired width in PIXELS of the image(s) when resizing.'
     )
 
     return parser.parse_args()
 
+
+def resize_image(image_path, output_path, new_name, method, height, width):
+    image_name = image_path.strip('/').split('/')[-1]
+    with Halo(
+        text=f'Resizing {image_name} ...',
+        spinner='dots',
+        text_color='white',
+        color='green'
+    ) as spinner:
+        path_regex = re.compile('^(.*/).*$')
+        name = image_name.split('.')[0]
+        extension = image_name.split('.')[1]
+        source = tinify.from_file(image_path)
+
+        if method == 'scale':
+            if height:
+                scaled_image = source.resize(
+                    method=method,
+                    height=int(height)
+                )
+
+                resized_name = f'{name}-resized-h{height}.{extension}'
+
+            elif width:
+                scaled_image = source.resize(
+                    method=method,
+                    width=int(width)
+                )
+
+                resized_name = f'{name}-resized-w{width}.{extension}'
+
+            if not output_path:
+                if not new_name:
+                    try:
+                        save_to_path = os.path.join(
+                            path_regex.search(image_path).group(1),
+                            resized_name
+                        )
+
+                    except AttributeError:
+                        save_to_path = resized_name
+
+                    scaled_image.to_file(save_to_path)
+
+                else:
+                    try:
+                        save_to_path = os.path.join(
+                            path_regex.search(image_path).group(1),
+                            new_name
+                        )
+
+                    except AttributeError:
+                        save_to_path = new_name
+
+                    scaled_image.to_file(save_to_path)
+
+            else:
+                if not new_name:
+                    save_to_path = os.path.join(output_path, image_name)
+                    scaled_image.to_file(save_to_path)
+
+                else:
+                    save_to_path = os.path.join(output_path, new_name)
+                    scaled_image.to_file(save_to_path)
+
+
 def main():
     args = parse_args()
+    verify_input(args.image, args.directory)
 
+
+    if args.output:
+        validate_output_path(args.output)
+        output_path = args.output
+
+    else:
+        output_path = False
+
+
+    if args.name:
+            new_name = args.name
+
+    else:
+        new_name = False
+
+
+    if args.directory:
+        validate_directory(args.directory)
+
+    elif args.image:
+        validate_image(args.image)
 
     # Compress Option
     if args.compress:
-        if not args.image and not args.directory:
-            print(
-                colored(
-                    'If compress option is chosen, you must specify -i or -d.',
-                    'red'
-                )
-            )
-
-            sys.exit(1)
-        
-
-        # Output Option
-        if args.output:
-            if not os.path.isdir(args.output):
-                print(
-                    colored(
-                        f'Output directory: {args.output} is not a directory.',
-                        'red'
-                    )
-                )
-
-                sys.exit(1)
-            
-            output_path = args.output
-
-        else:
-            output_path = False
-
-
-        # Name Option
-        if args.name:
-            new_name = args.name
-
-        else:
-            new_name = False
-
 
         # Directory Option
         if args.directory:
-            if not os.path.isdir(args.directory):
-                print(
-                    colored(
-                        f'{args.directory} is not a directory',
-                        'red'
-                    )
-                )
-
-                sys.exit(1)
-
-            else:
-                optimize_directory(args.directory, output_path)
-
+            optimize_directory(args.directory, output_path)
+                
         elif args.image:
-            image_path = args.image
-            extension = args.image.split('.')[-1].lower()
-            image_name = args.image.strip('/').split('/')[-1]
+            compress_image(args.image, output_path, new_name)
 
-            if not os.path.isfile(image_path):
-                print(
-                    colored(
-                        f'{args.image} is not a valid image path.',
-                        'red'
-                    )
-                )
+    # Resize Option
+    elif args.resize:
+        validate_resize_options(args)
+        validate_height_and_width(args)
 
-                sys.exit(1)
+        if args.image:
+            if args.scale:
+                method = 'scale'
 
-            
-            if extension not in valid_extensions:
-                print(
-                    colored(
-                        f'{image_name} is not a valid image, must be .jpg, .png, or .gif.',
-                        'red'
-                    )
-                )
+            elif args.fit:
+                method = 'fit'
 
-                sys.exit(1)
+            elif args.cover:
+                method = 'cover'
 
-        
-            compress_image(
-                image_path,
-                output_path,
-                new_name
-            )
+            elif args.thumb:
+                method = 'thumb'
+
+            resize_image(args.image, output_path, new_name, method, args.height, args.width)
+                
 
 
 if __name__ == '__main__':
